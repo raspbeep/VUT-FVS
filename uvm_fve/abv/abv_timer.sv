deff10e468c43eb5ca8031b4112e7ad7d27d8694
Pavel Kratochvil <kratochvilpavel.jr@gmail.com>
2025-04-24 08:11:15 +0200

    tidy up asserts and covers

--- uvm_fve/abv/abv_timer.sv
+++ uvm_fve/abv/abv_timer.sv
@@ -29,18 +29,11 @@ property cyclecntRegZeroWhenReset;
     @(posedge CLK) RST === RST_ACT_LEVEL |=> cnt_reg_d  === 0;
 endproperty
 
-// Property pr3: Checks if an out-of-range address results in OOR response next cycle
 property addrOORResponse;
     @(posedge CLK)
     disable iff ($isunknown(RST) || RST === RST_ACT_LEVEL)
     (ADDRESS > 8'h14) |-> ##1 (RESPONSE === CP_RSP_OOR);
 endproperty
-
-// counter register is not stable while waiting for IRQ
-sequence checkNotStable;
-    // cnt reg is not stable while waiting for IRQ
-    !$stable(cnt_reg_d) throughout (RST === RST_ACT_LEVEL && P_IRQ === 0);
-endsequence
 
 property signalsKnownInactiveReset;
     @(posedge CLK)
@@ -62,6 +55,7 @@ property prUnknownDataWrite;
         |-> (!$isunknown((DATA_IN))) && (!$isunknown((DATA_OUT)));
 endproperty
 
+// OOR has priority over UNALIGNED
 property prReadWriteOOR;
      @(posedge CLK)
      disable iff ($isunknown(RST) || RST === RST_ACT_LEVEL)
@@ -69,6 +63,7 @@ property prReadWriteOOR;
         |-> ##1 (RESPONSE === CP_RSP_OOR);
 endproperty
 
+// does not apply if the address is OOR
 property prReadWriteUnaligned;
      @(posedge CLK)
      disable iff ($isunknown(RST) || RST === RST_ACT_LEVEL)
@@ -90,6 +85,7 @@ property checkWriteReadSameAddr;
     |-> ##1 (DATA_OUT === v_data);
 endproperty
 
+// does not apply if the address is OOR or UNALIGNED
 property ackAfterCorrectAddr;
     @(posedge CLK)
     ( (REQUEST === CP_REQ_READ || REQUEST === CP_REQ_WRITE) && ADDRESS <= 8'h14 && ADDRESS[1:0] === 2'b00)
@@ -111,6 +107,7 @@ property errorResponseToResReq;
     ( RESPONSE === CP_RSP_ERROR );
 endproperty
 
+// this should never ever happen
 property noWaitResponse;
     @(posedge CLK) (RST === !RST_ACT_LEVEL) |-> !(RESPONSE === CP_RSP_WAIT);
 endproperty
@@ -120,6 +117,7 @@ property irqAfterCmpCntMatch;
         |-> ##1 (P_IRQ === 1);
 endproperty
 
+// does not apply if we change the mode during IRQ
 property clearCntAfterCmpCntMatchAutoRestart;
     @(posedge CLK) ((cnt_reg_d === cmp_reg_d) && (ctrl_reg_d === TIMER_CR_AUTO_RESTART)
         && !((REQUEST == CP_REQ_WRITE && DATA_IN != TIMER_CR_DISABLED)))
@@ -135,110 +133,193 @@ property incCntAfterCmpCntMatchContinuous;
         |-> ##1 ( cnt_reg_d === $past(cnt_reg_d) + 1 );
 endproperty
 
+// does not apply in cornercase
 property clearCntDisAfterCmpCntMatchOneShot;
     @(posedge CLK) ((cnt_reg_d === cmp_reg_d) && (ctrl_reg_d === TIMER_CR_ONESHOT)
-        && !((REQUEST == CP_REQ_WRITE && DATA_IN != TIMER_CR_DISABLED)) )
+        && !(REQUEST == CP_REQ_WRITE && DATA_IN != TIMER_CR_DISABLED))
         |-> ##1 (cnt_reg_d === 0 && ctrl_reg_d === TIMER_CR_DISABLED);
 endproperty
 
+// must be the current clock value
 property cycleLRead;
     @(posedge CLK) (ADDRESS === TIMER_CYCLE_L && REQUEST === CP_REQ_READ)
         |-> ##1 (DATA_OUT === cycle_cnt[31:0]);
 endproperty
 
+// must be the current clock value
 property cycleHRead;
     @(posedge CLK) (ADDRESS === TIMER_CYCLE_H && REQUEST === CP_REQ_READ)
         |-> ##1 (DATA_OUT === cycle_cnt[63:32]);
 endproperty
 
+// must be synchronous
 property cycleCntZeroDuringReset;
     @(posedge CLK) (RST === RST_ACT_LEVEL)
         |-> ##1 (cycle_cnt === 0);
 endproperty
 
+// --- Assertions and Covers ---
 
-
-// --- Assertions ---
-
-ResetRegCheck: assert property (asyncRegZeroWhenReset)
+a_ResetRegCheck: assert property (asyncRegZeroWhenReset)
 else
     begin
         `uvm_error("ABV_TIMER_ASSERT", "Assertion asyncRegZeroWhenReset FAILED: registers not resetting asynchronously with RST.")
     end
 
-ResetCycleCntRegCheck: assert property (cyclecntRegZeroWhenReset)
+a_ResetCycleCntRegCheck: assert property (cyclecntRegZeroWhenReset)
 else
     begin
         `uvm_error("ABV_TIMER_ASSERT", "Assertion cyclecntRegZeroWhenReset FAILED: cycle cnt not resetting synchronously with RST.")
     end
 
-addrOORResponseCheck: assert property (addrOORResponse)
+// --- asserts and covers from LAB4 ---
+
+a_addrOORResponseCheck: assert property (addrOORResponse)
 else
     begin
         `uvm_error("ABV_TIMER_ASSERT", $sformatf("Assertion addrOORResponse FAILED: OOR Address 0x%0h did not trigger OOR response (RESPONSE = %0d).", ADDRESS, RESPONSE))
     end
 
-signalsKnownInactiveResetCheck: assert property(signalsKnownInactiveReset)
+a_signalsKnownInactiveResetCheck: assert property(signalsKnownInactiveReset)
 else
     begin
-        `uvm_error("InputSignalNotUnknown", "Assertion signalsKnownInactiveReset FAILED: ADDRESS, REQUEST, RESPONSE, P_IRQ are unknown with inactive RST.")
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion signalsKnownInactiveReset FAILED: ADDRESS, REQUEST, RESPONSE, P_IRQ are unknown with inactive RST.")
     end
 
 // a   prUnknownDataRead
-a_prUnknownDataRead: assert property (prUnknownDataRead) else $error("ACK did not follow REQ within 2 cycles");
+a_prUnknownDataRead: assert property (prUnknownDataRead)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion prUnknownDataRead FAILED: DATA_IN or DATA_OUT has undefined values during reading.")
+    end
 
 // a   prUnknownDataWrite
-a_prUnknownDataWrite: assert property (prUnknownDataWrite) else $error("ACK did not follow REQ within 2 cycles");
+a_prUnknownDataWrite: assert property (prUnknownDataWrite)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion prUnknownDataWrite FAILED: DATA_IN or DATA_OUT has undefined values during writing.")
+    end
 
-// a+c prReadOOR
-a_prReadWriteOOR: assert property (prReadWriteOOR) else $error("ACK did not follow REQ within 2 cycles");
+// a+c prReadWriteOOR
+a_prReadWriteOOR: assert property (prReadWriteOOR)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion prReadWriteOOR FAILED: Writing or reading out of the allowed address range did not cause OOR response.")
+    end
+
 c_prReadWriteOOR: cover property (prReadWriteOOR);
 
 // a+c prReadWriteUnaligned
-a_prReadWriteUnaligned: assert property (prReadWriteUnaligned) else $error("ACK did not follow REQ within 2 cycles");
+a_prReadWriteUnaligned: assert property (prReadWriteUnaligned)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion prReadWriteUnaligned FAILED: Writing or reading to/from unaligned address did not cause UNALIGNED response.")
+    end
+
 c_prReadWriteUnaligned: cover property (prReadWriteUnaligned);
 
 // a+c checkWriteReadSameAddr
-a_checkWriteReadSameAddr: assert property (checkWriteReadSameAddr) else $error("ACK did not follow REQ within 2 cycles");
+a_checkWriteReadSameAddr: assert property (checkWriteReadSameAddr)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion checkWriteReadSameAddr FAILED: Read did not read fresh data after writing them in the previous clock.")
+    end
+
 c_checkWriteReadSameAddr: cover property (checkWriteReadSameAddr);
 
 // a+c ackAfterCorrectAddr
-a_ackAfterCorrectAddr: assert property (ackAfterCorrectAddr) else $error("ACK did not follow REQ within 2 cycles");
+a_ackAfterCorrectAddr: assert property (ackAfterCorrectAddr)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion ackAfterCorrectAddr FAILED: No ACK response after a valid address read/write.")
+    end
+
 c_ackAfterCorrectAddr: cover property (ackAfterCorrectAddr);
 
 // a+c idleResponseToNonReq
-a_idleResponseToNonReq: assert property (idleResponseToNonReq) else $error("ACK did not follow REQ within 2 cycles");
+a_idleResponseToNonReq: assert property (idleResponseToNonReq)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion idleResponseToNonReq FAILED: No IDLE response to NONE request.")
+    end
+
 c_idleResponseToNonReq: cover property (idleResponseToNonReq);
 
-a_errorResponseToResReq: assert property (errorResponseToResReq) else $error("ACK did not follow REQ within 2 cycles");
+// a+c errorResponseToResReq
+a_errorResponseToResReq: assert property (errorResponseToResReq)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion errorResponseToResReq FAILED: No ERROR response to RESERVED request.")
+    end
+
+c_errorResponseToResReq: cover property (errorResponseToResReq);
+
 // a   noWaitResponse
-a_noWaitResponse: assert property (noWaitResponse) else $error("ACK did not follow REQ within 2 cycles");
+a_noWaitResponse: assert property (noWaitResponse)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion noWaitResponse FAILED: There should not be any WAIT response (ever).")
+    end
 
 // a+c irqAfterCmpCntMatch
-a_irqAfterCmpCntMatch: assert property (irqAfterCmpCntMatch) else $error("ACK did not follow REQ within 2 cycles");
+a_irqAfterCmpCntMatch: assert property (irqAfterCmpCntMatch)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion irqAfterCmpCntMatch FAILED: IRQ should be (active) for exactly one clock after cmp_reg == cnt_reg match.")
+    end
+
 c_irqAfterCmpCntMatch: cover property (irqAfterCmpCntMatch);
 
 // a+c clearCntAfterCmpCntMatchAutoRestart
-a_clearCntAfterCmpCntMatchAutoRestart: assert property (clearCntAfterCmpCntMatchAutoRestart) else $error("ACK did not follow REQ within 2 cycles");
+a_clearCntAfterCmpCntMatchAutoRestart: assert property (clearCntAfterCmpCntMatchAutoRestart)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion clearCntAfterCmpCntMatchAutoRestart FAILED: cnt_reg did not reset after IRQ in AUTO_RESTART mode.")
+    end
+
 c_clearCntAfterCmpCntMatchAutoRestart: cover property (clearCntAfterCmpCntMatchAutoRestart);
 
 // a+c incCntAfterCmpCntMatchContinuous
-a_incCntAfterCmpCntMatchContinuous: assert property (incCntAfterCmpCntMatchContinuous) else $error("ACK did not follow REQ within 2 cycles");
+a_incCntAfterCmpCntMatchContinuous: assert property (incCntAfterCmpCntMatchContinuous)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion incCntAfterCmpCntMatchContinuous FAILED: cnt_reg did not increment after IRQ in CONTINUOUS mode.")
+    end
+
 c_incCntAfterCmpCntMatchContinuous: cover property (incCntAfterCmpCntMatchContinuous);
 
 // a+c clearCntDisAfterCmpCntMatchOneShot
-a_clearCntDisAfterCmpCntMatchOneShot: assert property (clearCntDisAfterCmpCntMatchOneShot) else $error("ACK did not follow REQ within 2 cycles");
+a_clearCntDisAfterCmpCntMatchOneShot: assert property (clearCntDisAfterCmpCntMatchOneShot)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion clearCntDisAfterCmpCntMatchOneShot FAILED: cnt_reg did not reset OR the mode was not set to DISABLED after IRQ in ONE_SHOT mode.")
+    end
 c_clearCntDisAfterCmpCntMatchOneShot: cover property (clearCntDisAfterCmpCntMatchOneShot);
 
 // a+c cycleLRead
-a_cycleLRead: assert property (cycleLRead) else $error("ACK did not follow REQ within 2 cycles");
+a_cycleLRead: assert property (cycleLRead)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion cycleLRead FAILED: cycle_l read did not return valid current clock value.")
+    end
+
 c_cycleLRead: cover property (cycleLRead);
 
 // a+c cycleHRead
-a_cycleHRead: assert property (cycleHRead) else $error("ACK did not follow REQ within 2 cycles");
+a_cycleHRead: assert property (cycleHRead)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion cycleLRead FAILED: cycle_h read did not return valid current clock value.")
+    end
+
 c_cycleHRead: cover property (cycleHRead);
 
 // a+c cycleCntZeroDuringReset
-a_cycleCntZeroDuringReset: assert property (cycleCntZeroDuringReset) else $error("ACK did not follow REQ within 2 cycles");
+a_cycleCntZeroDuringReset: assert property (cycleCntZeroDuringReset)
+else
+    begin
+        `uvm_error("ABV_TIMER_ASSERT", "Assertion cycleCntZeroDuringReset FAILED: cycle_cnt did not reset synchronously with RST signal.")
+    end
+
 c_cycleCntZeroDuringReset: cover property (cycleCntZeroDuringReset);
 endmodule : abv_timer
